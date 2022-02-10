08305651d7c9187949e761044f247fe3483201d6
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2022-02-11 01:00:27 +0530

    add first step

---
+++ simple_crud/src/redux/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    users: [],
+    user: {},
+    loading: false,
+};
+
+const userReducers = (state = initialState, action) => {
+    switch(action.type){
+        default: state;
+    }
+}
+
+export default userReducers; 
